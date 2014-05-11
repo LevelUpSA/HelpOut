4f3fb1440ea34417dea4d935be79cd5ff889b89f
Tokelo Seremane <seremane.t@gmail.com>
2014-05-11 20:15:00 +0200

    Changes made: Registration page (html), the regitration controller to allow users to register and the routes for regitration

--- public/js/controllers/RegistrationController.js
+++ public/js/controllers/RegistrationController.js
@@ -3,4 +3,35 @@ angular.module('RegistrationController', [])
 
 	$scope.tagline = 'I am inside registration controller';
 
+	$scope.createUser = function(){
+
+		$http.post('/api/registration', $scope.user)
+		.success(function(data){
+			$scope.registration = data;
+			$scope.user = {};
+		})
+		.error(function(err){
+			console.log('Error ' + err);
+		});
+	};
+
+	$scope.editUser = function(){
+		var user = {};
+		user['name'] = $scope.user.name;
+		user['email'] = $scope.user.email;
+		user['password'] = $scope.user.password;
+
+		$http.post(user.id)
+			.success(function(data){
+				$scope.registration = data;
+			});
+	}
+	$http.get('/api/registration')
+		.success(function(data){
+			$scope.registration = data;
+		})
+		.error(function(error){
+			console.log('Error: '+ error + 'Could not retrieve user')
+		});
+
 }]);
