ba07274fdb3c2fcb54a9121d263b953e7a72b5e0
Sydney Chauke <sydney>
2014-05-17 16:15:36 +0200

    Created event search filter

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,8 +1,9 @@
 angular.module('MainController', []).controller('MainController', ['$scope', '$http' , function($scope, $http) {
 
 	$scope.tagline = 'To the moon and back! Welcome to HelpOut App';
+    $scope.isAdvancedSearch = false;
 
-
+    $scope.advancedSearch
     $http.get('/api/events')
         .success(function(data){
             $scope.events = data;
---
+++ public/js/controllers/SearchController.js
@@ -0,0 +1,5 @@
+angular.module('SearchController', [])
+    .controller('SearchController', ['$scope', '$http' , function ($scope, $http) {
+
+
+    }]);
