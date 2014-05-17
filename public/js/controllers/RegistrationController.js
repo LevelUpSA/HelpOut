a2359c7fd52244303bc3f2935ccfb83d488b01e9
Suffixz21 <felixsean@ymail.com>
2014-05-17 13:18:28 +0200

    logon flag

--- public/js/controllers/RegistrationController.js
+++ public/js/controllers/RegistrationController.js
@@ -2,12 +2,14 @@ angular.module('RegistrationController', [])
     .controller('RegistrationController', ['$scope', '$http' , function($scope, $http) {
 
 	$scope.subTitle = 'Register to become a member';
+	$scope.isLogon = false;
 
 	$scope.createUser = function(){
 
 		$http.post('/api/registration', $scope.user)
 		.success(function(data){
 			$scope.user = data;
+			
 		})
 		.error(function(err){
 			console.log('Error ' + err);
