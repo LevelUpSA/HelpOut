8ff7a1642e480988ad8d8517e18bf0c4d43b3c6b
Sanzaristo <sandile.mnukwa@gmail.com>
2014-05-24 00:40:50 +0200

    created a button on registration form

--- public/js/controllers/RegistrationController.js
+++ public/js/controllers/RegistrationController.js
@@ -2,7 +2,7 @@ angular.module('RegistrationController', [])
     .controller('RegistrationController', ['$scope' , '$location', 'RegistrationService', function ($scope, $location, RegistrationService) {
 
         $scope.subTitle = 'Register to become a member';
-
+        $scope.isNextClicked = false;
         $scope.register = function () {
             RegistrationService.register($scope.user)
                 .then(function (user) {
@@ -12,5 +12,8 @@ angular.module('RegistrationController', [])
                 });
         };
 
+        $scope.goToNext = function(){
+            $scope.isNextClicked = true;
+        };
 
     }]);
