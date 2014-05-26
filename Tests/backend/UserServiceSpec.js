9646d95e6076ded939fae5283e79ff61c16a47ba
Sydney Chauke <sydney>
2014-05-26 23:39:50 +0200

    refactored callback tests

--- Tests/backend/UserServiceSpec.js
+++ Tests/backend/UserServiceSpec.js
@@ -1,62 +1,33 @@
-
 userService = require('../../app/api/UserService');
 
 describe('Tesing UserService', function () {
-    var errMsg, data, flag;
 
-    beforeEach(function(){
-        errMsg = '';
-        data = '';
-        flag = false;
-    });
-
-    it('should test empty email address', function () {
+    it('should test empty email address', function (done) {
         var detatils = getLoginDetails();
         delete detatils.email;
 
-        runs(function(){
-            userService.login(detatils, function(err, data){
-               errMsg = err;
-               flag = true;
-            });
-        }, 500);
-
-        waitsFor(function() {
-            return flag;
-        }, "Flag should be set", 750);
-
-        runs(function() {
-            expect(errMsg).toEqual('Email not entered');
+        userService.login(detatils, function (err, data) {
+            expect(err).toEqual('Email not entered');
+            expect(data).toBeFalsy();
+            return done();
         });
-
     });
 
-    it('should test empty password', function () {
+    it('should test empty password', function (done) {
         var detatils = getLoginDetails();
         delete detatils.password;
 
-        runs(function(){
-            userService.login(detatils, function(err, data){
-               errMsg = err;
-               flag = true;
-            });
-        }, 500);
-
-        waitsFor(function() {
-            return flag;
-        }, "Flag should be set", 750);
-
-        runs(function() {
-            expect(errMsg).toEqual('Password not entered');
+        userService.login(detatils, function (err, data) {
+            expect(err).toEqual('Password not entered');
+            expect(data).toBeFalsy();
+            return done();
         });
-
     });
 
     function getLoginDetails() {
-       return {
+        return {
             email: 'sydney@gmail.com',
             password: '123'
         };
     }
 });
-
